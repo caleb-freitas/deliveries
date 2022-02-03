3643f80781d69b75eb174bc8c157dddb59156d85
caleb-freitas <calebfreitas@tutanota.com>
2022-02-02 23:08:39 -0300

    chore: add authenticate route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
+import { AuthenticateClientController } from "./modules/account/authenticate-client/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/use-cases/create-client/CreateClientController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 
-routes.post("/client/", createClientController.handle);
+const authenticateClientController = new AuthenticateClientController();
+
+routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/client", createClientController.handle);
 
 export { routes };

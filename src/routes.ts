70d9ae89041958e0d86a27a8b6bd6dcfc0ae00d8
caleb-freitas <calebfreitas@tutanota.com>
2022-02-03 21:15:34 -0300

    chore: add create deliveryman route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { AuthenticateClientController } from "./modules/account/authenticate-client/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/use-cases/create-client/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliverymen/use-cases/create-deliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
@@ -9,7 +10,12 @@ const createClientController = new CreateClientController();
 
 const authenticateClientController = new AuthenticateClientController();
 
+const createDeliverymanController = new CreateDeliverymanController();
+
 routes.post("/authenticate", authenticateClientController.handle);
+
 routes.post("/client", createClientController.handle);
+
+routes.post("/deliveryman", createDeliverymanController.handle);
 
 export { routes };
